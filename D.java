3cee779ca439e4e89f8dd794c074ee74b976833e
Rafael Garcia <rafaeldesouza3@gmail.com>
2020-06-10 19:16:41 -0300

    Criação da classe D e seus métodos e atributos iniciais

---
+++ D.java
@@ -0,0 +1,33 @@
+/* Nome: Rafael de Souza Garcia
+Matricula: 201820003
+Turma: 10A */
+
+public class D {
+    private double D1;
+    private int D2;
+
+    public void setD1(double _D1) {
+        D1 = _D1;
+    }
+
+    public void setD2(int _D2) {
+        D2 = _D2;
+    }
+
+    public double getD1() {
+        return D1;
+    }
+
+    public int getD2() {
+        return D2;
+    }
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+}
+
