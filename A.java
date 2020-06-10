aaa9278c97589d4c3d7b239afc9da56d8260206c
rafael-garcia1 <66749622+rafael-garcia1@users.noreply.github.com>
2020-06-10 17:08:17 -0300

    Classes A e B adicionadas
    Classes A e B, com alguns atributos e métodos, criados de acordo com a instrução 2 do trabalho prático de gestão de versões.

---
+++ A.java
@@ -0,0 +1,32 @@
+/* Nome: Rafael de Souza Garcia
+Matricula: 201820003
+Turma: 10A */
+
+public class A {
+    private int A1;
+    private float A2;
+
+    public void setA1(int _A1) {
+        A1 = _A1;
+    }
+
+    public void setA2(float _A2) {
+        A2 = _A2;
+    }
+
+    public int getA1() {
+        return A1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void MA1() {
+        System.out.println("MA1");
+    }
+
+    public void MA2() {
+        System.out.println("MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,33 @@
+/* Nome: Rafael de Souza Garcia
+Matricula: 201820003
+Turma: 10A */
+
+public class B {
+    private int B1;
+    private float B2;
+
+    public void setB1(int _B1) {
+        B1 = _B1;
+    }
+
+    public void setB2(float _B2) {
+        B2 = _B2;
+    }
+
+    public int getB1() {
+        return B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void MB1() {
+        System.out.println("MB1");
+    }
+
+    public void MB2() {
+        System.out.println("MB2");
+    }
+}
+
