a379850d4c824bdfabe4d0391b53de9aa18615a3
Rafael Garcia <rafaeldesouza3@gmail.com>
2020-06-10 18:11:45 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,28 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    public void setC1(String _C1) {
+        C1 = _C1;
+    } 
+
+    public void setC2(int _C2) {
+        C2 = _C2;
+    }
+
+    public String getC1() {
+        return C1;
+    }
+
+    public int getC2() {
+        return C2;
+    }
+
+    public void MC1() {
+        System.out.println("MC1");
+    }
+
+    public void MC2() {
+        System.out.println("MC2");
+    }
+}
